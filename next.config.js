6a897b86a319ece57de18e8c990e0c02b4bc9069
9Pond <phonphiphat.prakopchip@gmail.com>
2026-07-19 16:13:26 +0700

    chore: fix build command and ignore errors

--- next.config.js
+++ next.config.js
@@ -1,14 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    // ยอมให้ Build ผ่านแม้ว่าจะมีข้อผิดพลาดเกี่ยวกับ TypeScript Type
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // ยอมให้ Build ผ่านแม้ว่าจะมีข้อผิดพลาดเกี่ยวกับ ESLint
-    ignoreDuringBuilds: true,
-  },
-  // ...ค่าคอนฟิกอื่นๆ ที่อาจจะมีอยู่เดิมในไฟล์ (ห้ามลบออก ให้คงไว้)
-};
-
-module.exports = nextConfig;
+module.exports = { typescript: { ignoreBuildErrors: true }, eslint: { ignoreDuringBuilds: true } }; 
